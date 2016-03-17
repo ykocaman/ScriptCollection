6ea30adfb97c8732b50fd6d4064fcfe11cdeb9dc
Yusuf Kocaman <yusuf.kocaman@okan.edu.tr>
2016-03-17 15:11:27 +0200

    table name

--- mysql/import_csv.sql
+++ mysql/import_csv.sql
@@ -12,4 +12,4 @@ CREATE TABLE tmp (
 csv dosyasini import eden kisim; table kismina tablo ismi ve /tmp/file.csv kismina ise dosyanin pathi verilmelidir. 
 TERMINATED BY kismindaki , yada ; dosyanin icerigine gore degistirilir. Virgul amerikada, noktali virgul ise ulkemiz ve avrupada ayrac olarak kullanilmaktadir.
 */
-LOAD DATA LOCAL INFILE '/tmp/file.csv' INTO TABLE table CHARACTER SET UTF8 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE '/tmp/file.csv' INTO TABLE tmp CHARACTER SET UTF8 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
