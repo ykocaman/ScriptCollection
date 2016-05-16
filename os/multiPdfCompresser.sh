905ccb5be5ea6aafbc51882ed92470e48434b544
yusuf kocaman <yusuf.kocaman@msn.com>
2016-05-16 14:17:55 +0300

    multi pdf compressor
    usage: ./multiPdfCompresser.sh targetDir

---
+++ os/multiPdfCompresser.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+output="output"
+
+find "$1" -type d -links 2 -exec mkdir -p "./$output/{}" \;
+
+find "$1" -type f -iname '*.pdf' -print0 | while read -d $'\0' file
+do
+	echo -n "$output/$file"
+	gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dDownsampleColorImages=true -dColorImageResolution=150 -dNOPAUSE  -dBATCH -sOutputFile="$output/$file" $file > /dev/null
+	echo " > OK"
+done
