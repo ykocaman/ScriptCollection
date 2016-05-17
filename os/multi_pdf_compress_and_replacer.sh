520ccda78f3b2d8107489ba07ffd434b7a4eeff8
yusuf kocaman <yusuf.kocaman@msn.com>
2016-05-17 15:50:04 +0300

    Multi PDF compress and replace

---
+++ os/multi_pdf_compress_and_replacer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# usage: ./multi_pdf_compress_and_replacer.sh targetDir/
+#
+
+originals="originals"
+
+find "$1" -type d -links 2 -exec mkdir -p "./$originals/{}" \;
+
+counter=0;
+find "$1" -type f -iname '*.pdf' -print0 | while read -d $'\0' file
+do
+	counter=$(($counter+1))
+	echo -n "$counter) $file"
+	mv $file "$originals/$file" > /dev/null 2>&1
+	gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dDownsampleColorImages=true -dColorImageResolution=150 -dNOPAUSE  -dBATCH -sOutputFile=$file "$originals/$file"  > /dev/null 2>&1 
+	echo "> OK"
+done
