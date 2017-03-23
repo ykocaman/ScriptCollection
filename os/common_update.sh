451a2255e0c2bcb87ec8c1afb370c17a733a13a2
ykocaman <ykocaman@users.noreply.github.com>
2017-03-23 08:46:12 +0200

    Update common_update.sh

--- os/common_update.sh
+++ os/common_update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 apt-get update
 apt-get upgrade -y
 apt-get autoremove -y
-apt-get autoclean -y
+apt-get autoclean
